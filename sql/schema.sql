d4c11c241e795128bcbeeb04c4f48d8a158cfa5b
LannyOakman <lannyoakman2009@gmail.com>
2025-08-07 19:29:40 +0530

    populating db with generated data

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS customer(
 
 CREATE TABLE IF NOT EXISTS product(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    p_name VARCHAR(64),
+    p_name VARCHAR(64) NOT NULL,
     p_type VARCHAR(32) NOT NULL,
     price decimal(5, 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sale(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    staff_id INT,
-    customer_id INT,
+    staff_id INT NOT NULL,
+    customer_id INT NOT NULL,
     ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (staff_id) REFERENCES staff(id),
     FOREIGN KEY (customer_id) REFERENCES customer(id)
@@ -28,9 +28,10 @@ CREATE TABLE IF NOT EXISTS sale(
 
 CREATE TABLE IF NOT EXISTS sale_product(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    sale_id INT,
-    product_id INT,
-    quantity INT,
+    sale_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
     FOREIGN KEY (sale_id) REFERENCES sale(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    Unique (sale_id, product_id)
 );
